45e23b6e5047ca3be6f4b0518cbb8f390a29b121
Eduardo H <eduardohv32@gmail.com>
2024-12-17 14:55:28 -0600

    modified:   src/components/API/API.csproj 	modified:   src/components/API/Function.cs 	modified:   src/components/main.js

--- src/components/API/Function.cs
+++ src/components/API/Function.cs
@@ -1,4 +1,4 @@
-using System.IO;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-const functionApi = 'http://localhost:7071/api/UpdateVisitorCount';
+const functionApiUrl = 'https://countresumeone.azurewebsites.net/api/UpdateVisitorCount?code=Q3ChqSipyzceOiTiimS4Z0BI867VCq4rmG4Dg9dkwZW4AzFucfGpAg%3D%3D'
+const localFunctionApi = 'http://localhost:7071/api/UpdateVisitorCount';
 
 
 const getVisitCount = () => {
 
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API");
