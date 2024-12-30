697e4286621743a9f09267b95413fe62d3771448
Eduardo H <eduardohv32@gmail.com>
2024-12-30 14:22:12 -0600

    deleted:    src/components/API/.gitignore 	deleted:    src/components/API/API.csproj 	deleted:    src/components/API/Function.cs 	deleted:    src/components/API/Properties/launchSettings.json 	deleted:    src/components/API/global.json 	deleted:    src/components/API/host.json 	modified:   src/components/main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-const functionApiUrl = 'https://countresumeone.azurewebsites.net/api/UpdateVisitorCount?code=Q3ChqSipyzceOiTiimS4Z0BI867VCq4rmG4Dg9dkwZW4AzFucfGpAg%3D%3D';
+const functionApiUrl = 'https://visitorcountone.azurewebsites.net/api/HttpTrigger1?code=GZpkQeYz8iJwF4AVZwSCSGHYOhKZrWJduPgRqJoy19tpAzFuOCelgA%3D%3D';
 const localFunctionApi = 'http://localhost:7071/api/UpdateVisitorCount';
 
 
