9a66a323c0cdd95581bf233a32e15b446e88f884
eduardohv32 <114089546+eduardohv32@users.noreply.github.com>
2025-05-01 12:27:06 -0600

    update profile photo URL to higher resolution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       <div className="container mx-auto px-4 relative">
         <div className="flex flex-col items-center text-center">
           <ProfilePhoto 
-            imageUrl="https://media.licdn.com/dms/image/v2/C4D03AQFczNivBu_lrQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1604863553382?e=1738800000&v=beta&t=1ADPpRgrSOYYf9X_CDcZcFUN-OuYx9p7PuACdUYlrgQ"
+            imageUrl="https://media.licdn.com/dms/image/v2/C4D03AQFczNivBu_lrQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1604863553382?e=1751500800&v=beta&t=51bSh5IAIwCus3oBi46bt1UkqRkTHXjl2K-LR4kK2p4"
             alt="Eduardo Henriquez - Cloud Engineer"
           />
           <div className="relative">
